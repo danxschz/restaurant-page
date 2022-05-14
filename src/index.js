ae9a4f9906e35daee8134cb8977d87e3d2b16ed5
danxschz <danxschz@gmail.com>
2022-05-13 20:38:37 -0400

    Add basic styles

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './normalize.css';
+import './style.scss';
+console.log('test')
+console.log('holaa')
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,24 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
