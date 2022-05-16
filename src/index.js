70ed168f7f15978a3c9ec268444e30dfa15102cb
Daniel Sanchez <danxschz@gmail.com>
2022-05-16 14:09:40 -0400

    Build and style contact section

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import html from "./index.html";
 import shrimpWonton from './shrimp-wonton.png';
 import chickenRamen from './chicken-ramen.jpg';
 import tonkotsuRed from './tonkotsu-red.png';
+import location from './location.png';
 
-//document.title = "test"
 
 const setImageModals = () => {
   let modal = document.querySelector('.modal');
-  var modalContent = document.querySelector('.modal__content');
+  let modalContent = document.querySelector('.modal__content');
   let imgs = document.querySelectorAll('main img');
   imgs.forEach(image => {
     image.addEventListener('click', () => {
@@ -83,6 +83,21 @@ const buildMenu = () => {
 
     main.appendChild(dishContainer);
   }
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  let close = document.createElement('div');
+  close.classList.add('close');
+  close.textContent = '×';
+  modal.appendChild(close);
+
+  let modalContent = document.createElement('img');
+  modalContent.classList.add('modal__content');
+  modal.appendChild(modalContent);
+
+  main.appendChild(modal);
+  setImageModals();
+  document.title = 'Menu - Yumi';
 }
 
 const removeSelected = () => {
@@ -98,7 +113,6 @@ menuButton.addEventListener('click', ()=> {
   buildMenu();
   removeSelected();
   menuButton.classList.add('selected');
-
 })
 
 let homeButton = document.querySelector('.home-button')
@@ -108,4 +122,88 @@ homeButton.addEventListener('click', ()=> {
   homeButton.classList.add('selected');
 })
 
+const buildContact = () => {
+  let main = document.querySelector('main');
+  main.removeAttribute('class');
+  main.classList.add('contact');
+
+  let leftSection = document.createElement('div');
+  leftSection.classList.add('contact__left-section');
+
+  let japPhrase = document.createElement('div');
+  japPhrase.classList.add('jap-phrase');
+  japPhrase.textContent = 'コンタクト';
+  leftSection.appendChild(japPhrase);
+
+  let wavyLine = document.createElement('div');
+  wavyLine.classList.add('wavy-line');
+  wavyLine.textContent = '∿∿∿∿∿∿∿';
+  leftSection.appendChild(wavyLine);
+
+  let title = document.createElement('div');
+  title.classList.add('contact__title');
+  
+  let line = document.createElement('div');
+  line.textContent = 'CONTACT';
+  title.appendChild(line);
+
+  line = document.createElement('div');
+  line.textContent = 'INFORMATION';
+  title.appendChild(line);
+
+  leftSection.appendChild(title);
+  main.appendChild(leftSection);
+
+
+  let rightSection = document.createElement('div');
+  rightSection.classList.add('contact__right-section');
+
+
+  let phones = document.createElement('div');
+  phones.classList.add('phones');
+
+  let phonesTitle = document.createElement('div');
+  phonesTitle.classList.add('phones__title');
+  phonesTitle.textContent = 'Phones';
+  phones.appendChild(phonesTitle);
+
+  for (let i = 0; i < 2; i++) {
+    let number = document.createElement('div');
+    number.classList.add('phones__number');
+    number.textContent = `(123) 4${i}6 ${i*2}8${i+3}${i}`;
+    phones.appendChild(number);
+  }
+  rightSection.appendChild(phones);
+
+  let address = document.createElement('div');
+
+  let addressTitle = document.createElement('div');
+  addressTitle.classList.add('address__title');
+  addressTitle.textContent = 'Address';
+  address.appendChild(addressTitle);
+
+
+  let addressDescription = document.createElement('div');
+  addressDescription.classList.add('address__description');
+  addressDescription.textContent = 'Hollywood Boulevard 42, Los Angeles, USA'
+  address.appendChild(addressDescription);
+  rightSection.appendChild(address);
+
+  let map = document.createElement('img');
+  map.src = location;
+  rightSection.appendChild(map);
+
+
+  main.appendChild(rightSection);
+
+  document.title = 'Contact Us - Yumi';
+}
+
+let contactButton = document.querySelector('.contact-button');
+contactButton.addEventListener('click', ()=> {
+  clearMain();
+  buildContact();
+  removeSelected();
+  contactButton.classList.add('selected');
+})
 
