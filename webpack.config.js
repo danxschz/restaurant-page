e01751b98b160125f2471254a1108e6135282d5d
Daniel Sanchez <danxschz@gmail.com>
2022-08-02 00:18:06 -0400

    Add new favicon and update webpack config to keep src folder structure on dist

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: (pathData) => {
+      const filepath = path
+        .dirname(pathData.filename)
+        .split("/")
+        .slice(1)
+        .join("/");
+      return `${filepath}/[name].[hash][ext][query]`;
+    },
     clean: true,
   },
   devtool: 'inline-source-map',
